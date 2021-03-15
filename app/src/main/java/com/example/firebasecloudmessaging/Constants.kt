9a1ffa7d65b72281e3902a0ab0bf63174fa140d1
adeangafrio <adean6749@gmail.com>
2021-03-15 11:23:17 +0700

    Initial commit

---
+++ app/src/main/java/com/example/firebasecloudmessaging/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.firebasecloudmessaging
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://fcm.googleapis.com"
+        const val SERVER_KEY = "AAAAb8u0IRs:APA91bHRuJyS0k5LQhOOT2-1fLnqpoXDeX0VRIwdTbjVmPFXNA0bn4B8K64nLDa_r3hhvQJS13sebvv5vn4xUMmqDsxXMMeJTf3Z_hEzsanpaW6U1JKARIRsBkucp25LD1bX7IGxoZPi"
+        const val CONTENT_TYPE = "application/json"
+    }
+
+}
